625de103118ad320ef45a0f4b11a5a6c0bf20102
Mykhailo Mulko <mulko.mykhailo@google.com>
2021-08-15 17:37:42 +0300

    Version 0.1.1 | Added link to PythonAnywhere

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,4 +1,4 @@
 bot_token = "1934178522:AAF8HvlwnndqTDan1ElpCii9orjgOlcr8KI"
 bot_user_name = "yizhak_corporate_bot"
-URL = ""
+URL = "https://mulkostorm.pythonanywhere.com/"
 database_url = 'https://yizhak-533ce-default-rtdb.europe-west1.firebasedatabase.app/'
